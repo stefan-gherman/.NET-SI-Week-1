7a5d56d56c1bef4dda894228ed3440f18f3e3aa2
scgheman <scgheman@gmail.com>
2020-06-11 18:38:43 +0300

    Create employee class

---
+++ Exercise 1/Exercise 1/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    public class Employee : Person
+    {
+        private int _salary;
+        private string _profession;
+        
+        public Employee()
+        {
+
+        }
+        public Employee(string name, DateTime birthDate, Genders gender, int salary, string profession) : base(name, birthDate, gender)
+        {
+            _salary = salary;
+            _profession = profession;
+        } 
+        public int Salary
+        {
+            get { return _salary; }
+            set { _salary = value; }
+        }
+
+        public string Profession
+        {
+            get { return _profession; }
+            set { _profession = value; }
+        }
+
+        public override string ToString() => base.ToString().Remove(base.ToString().Length - 1) + $" works as {Profession}, earns {Salary}";
+
+    }
+}
